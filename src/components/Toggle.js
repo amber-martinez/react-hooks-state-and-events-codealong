c6d2ecfb1d39ecb92aad9f20af9831fbbfcd1dd0
Amber <martinez.amber1028@gmail.com>
2022-03-21 21:15:36 -0700

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [button, setButton] = useState(false)
+
+  function setter() {
+    setButton((button) => !button)
+  }
+  //   if (button === true) {
+  //     setButton(false)
+  //   } else {
+  //     setButton(true)
+  //   }
+  // }
+
+  const color = button ? "red" : "white";
+
+
+  return <button 
+  style={{ background: color }}
+  onClick={setter}>
+  {button ? "ON" : "OFF"}
+  </button>;
 }
 
 export default Toggle;
